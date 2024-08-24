f205727e6520ca87c74c2986fe3b606f641a5ba1
fathyali11 <fathy.ali8ali@gmail.com>
2024-08-25 00:13:10 +0300

    add view component to now number of users

---
+++ ShopEgypt.Web/ViewComponents/UsersViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ShopEgypt.Web.ViewComponents
+{
+    public class UsersViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UsersViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var numberOfUsers=_unitOfWork.ApplicaionUserRepository.GetAll().Count();
+            return View("UsersCount", numberOfUsers);
+        }
+
+    }
+}
