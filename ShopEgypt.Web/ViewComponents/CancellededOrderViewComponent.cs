dd92b7bf4fe1b8fe10ea03b331e6d424bb731ebf
fathyali11 <fathy.ali8ali@gmail.com>
2024-08-25 00:18:16 +0300

    return number of cancelled order

---
+++ ShopEgypt.Web/ViewComponents/CancellededOrderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopEgypt.Web.ViewComponents
+{
+    public class CancellededOrderViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CancellededOrderViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var numberOfOrders = _unitOfWork.OrderHeaderReposittory.GetAll(x=>x.OrderStatus==MyStatus.StatusCancelled).Count();
+            return View("OrderCount", numberOfOrders);
+        }
+    }
+}
---
+++ ShopEgypt.Web/wwwroot/js/Orders/CancelledOrders.js
@@ -0,0 +1,25 @@
+﻿var dtable;
+
+$(document).ready(function () {
+    loadData();
+});
+
+function loadData() {
+    dtable = $("#MyTable").DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetCancelledOrders"
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "email" },
+            { "data": "phoneNumber" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return '<a href="/Admin/Order/Details?OrderId=' + data + '" class="btn btn-success">Details</a>';
+                }
+            }
+        ]
+    });
+}
