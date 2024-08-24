53b74a3b004ef4e330cf717e7b0b13e6b72a60c0
fathyali11 <fathy.ali8ali@gmail.com>
2024-08-25 00:17:03 +0300

    shipped view component

---
+++ ShopEgypt.Web/ViewComponents/ShippedOrderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopEgypt.Web.ViewComponents
+{
+    public class ShippedOrderViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShippedOrderViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var numberOfOrders = _unitOfWork.OrderHeaderReposittory.GetAll(x=>x.OrderStatus==MyStatus.StatusShipped).Count();
+            return View("OrderCount", numberOfOrders);
+        }
+    }
+}
