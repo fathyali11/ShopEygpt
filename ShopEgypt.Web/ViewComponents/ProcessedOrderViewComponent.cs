897eca477bc1af0b5fcc4a163636c5fe498862c8
fathyali11 <fathy.ali8ali@gmail.com>
2024-08-25 00:17:39 +0300

    return number of processing orders

---
+++ ShopEgypt.Web/ViewComponents/ProcessedOrderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopEgypt.Web.ViewComponents
+{
+    public class ProcessedOrderViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProcessedOrderViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var numberOfOrders = _unitOfWork.OrderHeaderReposittory.GetAll(x=>x.OrderStatus==MyStatus.StatusInProcess).Count();
+            return View("OrderCount", numberOfOrders);
+        }
+    }
+}
