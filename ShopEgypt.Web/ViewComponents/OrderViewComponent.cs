bbbe26477bc7797b0484e1f9780baa455a177436
fathyali11 <fathy.ali8ali@gmail.com>
2024-08-25 00:16:43 +0300

    to return number of orders

---
+++ ShopEgypt.Web/ViewComponents/OrderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopEgypt.Web.ViewComponents
+{
+    public class OrderViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var numberOfOrders=_unitOfWork.OrderHeaderReposittory.GetAll().Count();
+            return View("OrderCount", numberOfOrders);
+        }
+    }
+}
