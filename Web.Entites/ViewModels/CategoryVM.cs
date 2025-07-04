d40d528fe4be01219884a6e27f44262586ac795b
fathy <fathy@FATHYALI>
2025-07-04 19:49:35 +0300

    add categoryVM for adding a category

---
+++ Web.Entites/ViewModels/CategoryVM.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Web.Entites.ViewModels;
+public class CategoryVM
+{
+    public string Name { get; set; } = string.Empty;
+    public IFormFile? ImageCover { get; set; }
+}
