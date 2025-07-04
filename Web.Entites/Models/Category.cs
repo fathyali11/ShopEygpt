12c60c6e14968e0d63fd5b6dc1996b479eebb782
fathy <fathy@FATHYALI>
2025-07-04 19:49:58 +0300

    remove unused code from category class

--- Web.Entites/Models/Category.cs
+++ Web.Entites/Models/Category.cs
@@ -5,12 +5,11 @@ namespace Web.Entites.Models
 	public class Category
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		//public string ImageName {  get; set; }
+		public string Name { get; set; }=string.Empty;
 		public DateTime CreatedDate { get; set; }= DateTime.Now;
 		[NotMapped]
 		public IFormFile ?ImageCover { get; set; }
 		[ValidateNever]
-		public string ImageName {  get; set; }
+		public string ImageName {  get; set; }= string.Empty;
 	}
 }
