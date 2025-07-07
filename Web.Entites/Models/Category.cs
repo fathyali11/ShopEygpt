5f0c878d52d84164ed2a87df85330c0a56c17222
fathy <fathy@FATHYALI>
2025-07-08 00:50:57 +0300

    remove unused data from category entity

--- Web.Entites/Models/Category.cs
+++ Web.Entites/Models/Category.cs
@@ -1,16 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Web.Entites.Models
+﻿namespace Web.Entites.Models
 {
 	public class Category
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		//public string ImageName {  get; set; }
-		public DateTime CreatedDate { get; set; }= DateTime.Now;
-		[NotMapped]
-		public IFormFile ?ImageCover { get; set; }
-		[ValidateNever]
-		public string ImageName {  get; set; }
+		public string Name { get; set; } = string.Empty;
+		public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
+		public string ImageName { get; set; } = string.Empty;
 	}
 }
