bb3248bed67d01c48fb246f98123a482945a1171
fathy <fathy@FATHYALI>
2025-07-08 01:19:42 +0300

    rename imageUrl to imageName

--- Web.Entites/ViewModels/CategoryVMs/CategoryResponse.cs
+++ Web.Entites/ViewModels/CategoryVMs/CategoryResponse.cs
@@ -3,6 +3,6 @@ public class CategoryResponse
 {
     public int Id { get; set; }
     public string Name { get; set; }=string.Empty;
-    public string ImageUrl { get; set; } = string.Empty;
+    public string ImageName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
 }
