40fc30040af5bb0861a09ed713a0f766b600e17a
fathy <fathy@FATHYALI>
2025-07-08 01:07:00 +0300

    enhance unit of work class

--- Web.DataAccess/Repositories/UnitOfWork.cs
+++ Web.DataAccess/Repositories/UnitOfWork.cs
@@ -1,32 +1,14 @@
-﻿
-
-namespace Web.DataAccess.Repositories
+﻿namespace Web.DataAccess.Repositories;
+public class UnitOfWork(ApplicationDbContext _context) : IUnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
-    {
-        private readonly ApplicationDbContext _context;
+    public ICategoryRepository CategoryRepository{ get; private set; }= new CategoryRepository(_context);
+    public IProductRepository ProductRepository { get; private set; }= new ProductRepository(_context);
+    public IShoppingCartRepository ShoppingCartRepository { get; private set; }= new ShoppingCartRepository(_context);
+    public IOrderHeaderReposittory OrderHeaderReposittory {  get; private set; }= new OrderHeaderReposittory(_context);
+    public IOrderDetailReposittory OrderDetailReposittory{ get; private set; }= new OrderDetailReposittory(_context);
+    public IApplicaionUserRepository  ApplicaionUserRepository {  get; private set; }= new ApplicationUserRepository(_context);
 
-        public ICategoryRepository CategoryRepository{ get; private set; }
-        public IProductRepository ProductRepository { get; private set; }
-        public IShoppingCartRepository ShoppingCartRepository { get; private set; }
-        public IOrderHeaderReposittory OrderHeaderReposittory {  get; private set; }
-        public IOrderDetailReposittory OrderDetailReposittory{ get; private set; }
-        public IApplicaionUserRepository  ApplicaionUserRepository {  get; private set; }
-
-        public UnitOfWork(ApplicationDbContext context)
-        {
-            this._context = context;
-            CategoryRepository=new CategoryRepository(_context);
-            ProductRepository = new ProductRepository(_context);
-            ShoppingCartRepository = new ShoppingCartRepository(_context);
-            OrderHeaderReposittory=new OrderHeaderReposittory(_context);
-            OrderDetailReposittory= new OrderDetailReposittory(_context);
-            ApplicaionUserRepository=new ApplicationUserRepository(_context);
-        }
-
-        public int Save()
-        {
-            return _context.SaveChanges();
-        }
-    }
+    public async Task<int> SaveAsync()
+        => await _context.SaveChangesAsync();
 }
+
