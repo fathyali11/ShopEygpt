ad8bb680e2c298ea895dee3f747d632a55b3c779
fathy <fathy@FATHYALI>
2025-07-08 01:20:02 +0300

    add category mapping using mapster

---
+++ Web.Entites/Mappings/CategoryMapping.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using Web.Entites.Models;
+using Web.Entites.ViewModels.CategoryVMs;
+namespace Web.Entites.Mappings;
+public class CategoryMapping
+{
+    public static void RegisterMappings()
+    {
+        TypeAdapterConfig<CreateCategoryVM, Category>.NewConfig()
+            .Map(dest => dest.ImageName, src => src.Image.FileName);
+
+        TypeAdapterConfig<Category, CategoryResponse>.NewConfig();
+    }
+}
