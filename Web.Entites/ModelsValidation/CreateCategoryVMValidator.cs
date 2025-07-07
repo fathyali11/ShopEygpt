040d7ec8d589fe418dd4da6341021454fb49f27a
fathy <fathy@FATHYALI>
2025-07-08 02:14:09 +0300

    validator class for category

---
+++ Web.Entites/ModelsValidation/CreateCategoryVMValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Web.Entites.ViewModels.CategoryVMs;
+
+namespace Web.Entites.ModelsValidation;
+public class CreateCategoryVMValidator:AbstractValidator<CreateCategoryVM>
+{
+    public CreateCategoryVMValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .MaximumLength(30).WithMessage("Category name cannot exceed 30 characters.");
+        RuleFor(x => x.Image)
+            .NotNull().WithMessage("Image is required.")
+            .Must(ImageSignatureValidator.IsValidImage).WithMessage("Invalid image format. Only .jpg, .jpeg, .png are allowed.");
+    }
+}
