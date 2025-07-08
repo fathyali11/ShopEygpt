f36062a5a308248f5499e806c4fc0e2ea72b3fae
fathy <fathy@FATHYALI>
2025-07-08 17:33:47 +0300

    remove unused code

--- Web.DataAccess/Utilities/SD.cs
+++ Web.DataAccess/Utilities/SD.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Web.DataAccess.Utilities
+﻿namespace Web.DataAccess.Utilities
 {
     public static class SD
     {
