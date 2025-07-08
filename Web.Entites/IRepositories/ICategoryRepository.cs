dacf873482228d9c87cb84f0115a3d23a2b33b90
fathy <fathy@FATHYALI>
2025-07-08 09:08:24 +0300

    enhance create category

--- Web.Entites/IRepositories/ICategoryRepository.cs
+++ Web.Entites/IRepositories/ICategoryRepository.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using OneOf;
 using Web.Entites.Models;
+using Web.Entites.ViewModels;
 using Web.Entites.ViewModels.CategoryVMs;
 
 namespace Web.Entites.IRepositories
 {
     public interface ICategoryRepository : IGenericRepository<Category>
     {
-        Task AddCategoryAsync(CreateCategoryVM categoryVM);
+        Task<OneOf<List<ValidationError>, bool>> AddCategoryAsync(CreateCategoryVM categoryVM, CancellationToken cancellationToken = default);
         Task DeleteCategoryAsync(Category category);
         Task UpdateCategoryAsync(Category model);
         Task<IEnumerable<SelectListItem>> CategorySelectListAsync();
