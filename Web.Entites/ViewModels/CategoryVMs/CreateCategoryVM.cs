a51ef8a8bccc737481b6f2ee7d8dc177251889de
fathy <fathy@FATHYALI>
2025-07-08 17:18:28 +0300

    add data annotation

--- Web.Entites/ViewModels/CategoryVMs/CreateCategoryVM.cs
+++ Web.Entites/ViewModels/CategoryVMs/CreateCategoryVM.cs
@@ -1,4 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 
-namespace Web.Entites.ViewModels.CategoryVMs;
-public record CreateCategoryVM(string Name,IFormFile Image);
+public record CreateCategoryVM
+(
+    [Required(ErrorMessage = "Category name is required.")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Category name must be between 3 and 30 characters.")]
+    string Name ,
+
+    [Required(ErrorMessage = "Image is required.")]
+    IFormFile Image 
+);
