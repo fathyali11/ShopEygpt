f86600616dc49264b79d6554e24e5eaad165b9f0
fathy <fathy@FATHYALI>
2025-07-09 18:34:43 +0300

    make IProductRepo async

--- Web.Entites/IRepositories/IProductRepository.cs
+++ Web.Entites/IRepositories/IProductRepository.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Web.Entites.Models;
-using Web.Entites.ViewModels;
+﻿using Web.Entites.Models;
+using Web.Entites.ViewModels.ProductVMs;
 
 namespace Web.Entites.IRepositories
 {
-    public interface IProductRepository:IGenericRepository<Product>
+    public interface IProductRepository : IGenericRepository<Product>
     {
-        void Update(ProductVMEdit model);
-        void AddProductVM(ProductVMCreate Model);
-        void DeleteWithImage(Product product);
+        Task UpdateProductAsync(EditProductVM model);
+        Task AddProductAsync(CreateProductVM model);
+        Task DeleteProductAsync(int id);
     }
 }
