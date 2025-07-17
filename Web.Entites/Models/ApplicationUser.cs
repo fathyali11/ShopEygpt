95131318a4b73f8bea70c2b9194b673b04288821
fathy <fathy@FATHYALI>
2025-07-18 02:19:42 +0300

    refactor application user

--- Web.Entites/Models/ApplicationUser.cs
+++ Web.Entites/Models/ApplicationUser.cs
@@ -6,10 +6,7 @@ namespace Web.Entites.Models
 {
     public class ApplicationUser:IdentityUser
     {
-        public string Name {  get; set; }
-        public string Phone {  get; set; }
-        public string City {  get; set; }
-        [ValidateNever]
-        public string Role { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName {  get; set; }= string.Empty;
     }
 }
