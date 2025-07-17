c245d062c7ae67b2ef8db31d6219cc73e42e1bdc
fathy <fathy@FATHYALI>
2025-07-18 02:26:55 +0300

    add RegisterVM

---
+++ Web.Entites/ViewModels/UsersVMs/RegisterVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Entites.ViewModels.UsersVMs;
+public record RegisterVM(
+    string FirstName,
+    string LastName,
+    string Email,
+    string Password,
+    string ConfirmPassword
+    );
