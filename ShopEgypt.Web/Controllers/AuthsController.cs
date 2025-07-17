f176dcb929415816b03c16eb8cbf687b3ec63103
fathy <fathy@FATHYALI>
2025-07-18 02:56:04 +0300

    create authsController and add register

---
+++ ShopEgypt.Web/Controllers/AuthsController.cs
@@ -0,0 +1,22 @@
+﻿using Web.Entites.ViewModels.UsersVMs;
+
+namespace ShopEgypt.Web.Controllers;
+public class AuthsController(IAuthRepository _authRepository) : Controller
+{
+    [HttpGet]
+    public IActionResult Register() => View();
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM registerVM,CancellationToken cancellationToken)
+    {
+        var result = await _authRepository.RegisterAsync(registerVM, cancellationToken);
+        if (result.IsT1)
+            return RedirectToAction("Index", "Home");
+
+        var errors = result.AsT0;
+        foreach (var item in errors)
+            ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+        return View(registerVM);
+
+    }
+}
