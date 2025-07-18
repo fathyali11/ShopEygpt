c81316efe86ed4ccdf196e4372b4f05daa75289f
fathy <fathy@FATHYALI>
2025-07-19 01:29:52 +0300

    add AddToCart and it's implementation

---
+++ Web.DataAccess/Repositories/CartRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.DataAccess.Repositories
+{
+    public class CartRepository(ApplicationDbContext context) : GenericRepository<Cart>(context), ICartRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+        public async Task AddToCartAsync(string userId, int productId, CancellationToken cancellationToken = default)
+        {
+            var cart=await _context.Carts
+                .Include(x=>x.CartItems)
+                .FirstOrDefaultAsync(x=>x.UserId==userId,cancellationToken);
+
+            if(cart==null)
+            {
+                cart = new Cart { UserId = userId, CartItems = [] };
+                await _context.AddAsync(cart, cancellationToken);
+            }
+
+            var cartItem=await _context.CartItems
+                .FirstOrDefaultAsync(x=>x.ProductId==productId,cancellationToken);
+
+            if (cartItem is not null)
+                cartItem.Count++;
+            else
+            {
+                cart.CartItems.Add(new CartItem { ProductId=productId,Count=1 });
+            }
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        public decimal GetTotalPrice(IEnumerable<Cart> shoppingCarts)
+        {
+            decimal totalPrice = 0;
+            foreach(var item in shoppingCarts)
+            {
+                totalPrice += (item.Product.Price * item.Count);
+            }
+            return totalPrice;
+        }
+    }
+}
---
+++ Web.Entites/IRepositories/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using Web.Entites.Models;
+
+namespace Web.Entites.IRepositories
+{
+    public interface ICartRepository:IGenericRepository<Cart>
+    {
+        decimal GetTotalPrice(IEnumerable<Cart> shoppingCarts);
+        Task AddToCartAsync(string userId, int productId, CancellationToken cancellationToken = default);
+    }
+}
