ed47979adc7ae153bc23a6c9cb3aee7bd1d38dc0
fathy <fathy@FATHYALI>
2025-07-20 23:31:57 +0300

    add image name for cartItem

--- Web.Entites/Models/CartItem.cs
+++ Web.Entites/Models/CartItem.cs
@@ -4,6 +4,7 @@ public class CartItem
 {
     public int Id {  get; set; }
     public int ProductId {  get; set; }
+    public string ImageName { get; set; } = string.Empty;
     public int CartId {  get; set; }
     public int Count {  get; set; }
     public decimal TotalPrice => Product.Price * Count;
