7f9113c8a09adf70d409ab8d24aa81cc302d0228
fathy <fathy@FATHYALI>
2025-07-20 13:55:23 +0300

    add returnUrl

--- ShopEgypt.Web/Controllers/AuthsController.cs
+++ ShopEgypt.Web/Controllers/AuthsController.cs
@@ -24,22 +24,33 @@ public class AuthsController(IAuthRepository _authRepository,
     }
 
     [HttpGet]
-    public IActionResult Login() => View();
+    public IActionResult Login(string? returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        return View();
+    }
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Login(LoginVM loginVM, CancellationToken cancellationToken)
+    public async Task<IActionResult> Login(LoginVM loginVM, string? returnUrl = null, CancellationToken cancellationToken = default)
     {
         var result = await _authRepository.LoginAsync(loginVM, cancellationToken);
+
         if (result.IsT1)
+        {
+            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+
             return RedirectToAction("Index", "Home");
+        }
 
         var errors = result.AsT0;
         foreach (var item in errors)
             ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
 
+        ViewData["ReturnUrl"] = returnUrl;
         return View(loginVM);
-
     }
+
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Logout()
