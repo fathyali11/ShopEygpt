633c502d2640c1af9f0a214ded54ecfa18264348
fathy <fathy@FATHYALI>
2025-07-21 19:45:11 +0300

    add product name for cart item

--- Web.Entites/Models/CartItem.cs
+++ Web.Entites/Models/CartItem.cs
@@ -4,6 +4,7 @@ public class CartItem
 {
     public int Id {  get; set; }
     public int ProductId {  get; set; }
+    public string ProductName { get; set; } = string.Empty;
     public string ImageName { get; set; } = string.Empty;
     public int CartId {  get; set; }
     public int Count {  get; set; }
