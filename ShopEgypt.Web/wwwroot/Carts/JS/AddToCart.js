2f2e7a0e944f5f56264b2c3f0d49714666472a47
fathy <fathy@FATHYALI>
2025-07-25 01:12:25 +0300

    update cart count in home page and remove duplicated code

--- ShopEgypt.Web/wwwroot/Carts/JS/AddToCart.js
+++ ShopEgypt.Web/wwwroot/Carts/JS/AddToCart.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    updateCartCount(); 
+
     $(document).on("click", ".add-to-cart", function (e) {
         e.preventDefault();
 
@@ -8,24 +10,6 @@
         const price = $(this).data("price");
         const count = $(this).data("count");
         const token = $('input[name="__RequestVerificationToken"]').val();
-
-        //console.log('Product ID:', productId);
-        //console.log('Product Name:', productName);
-        //console.log('Image Name:', imageName);
-        //console.log('Price:', price);
-        //console.log('Count:', count);
-        //// Log the CSRF token for debugging purposes
-        //if (token === undefined || token === null || token.trim() === '') {
-        //    console.error('CSRF token is missing or invalid.');
-        //    Swal.fire({
-        //        icon: 'error',
-        //        title: 'Error',
-        //        text: 'CSRF token is missing or invalid. Please refresh the page.'
-        //    });
-        //    return;
-        //} else {
-        //    console.log('CSRF Token:', token);
-        //}
 
         $.ajax({
             url: `/Cart/Add`,
@@ -48,6 +32,8 @@
                         text: response.message,
                         timer: 1500,
                         showConfirmButton: false
+                    }).then(() => {
+                        updateCartCount(); 
                     });
                 } else {
                     Swal.fire({
@@ -59,7 +45,6 @@
             },
             error: function (xhr) {
                 const returnUrl = window.location.pathname + window.location.search;
-                console.log('Redirecting to login with return URL:', returnUrl);
                 if (xhr.status === 401 || xhr.status === 403) {
                     Swal.fire({
                         icon: 'warning',
@@ -68,7 +53,6 @@
                     }).then(() => {
                         window.location.href = `/Auths/Login?returnUrl=${returnUrl}`;
                     });
-                    
                 } else {
                     Swal.fire({
                         icon: 'error',
@@ -80,3 +64,16 @@
         });
     });
 });
+
+function updateCartCount() {
+    $.ajax({
+        url: "/Cart/Count",
+        type: "GET",
+        success: function (data) {
+            $("#cart-count").text("(" + data.count + ")");
+        },
+        error: function (xhr, status, error) {
+            console.error("Error fetching cart count:", error);
+        }
+    });
+}
