743102cd48aeeaeb94c7a0c2e2f7e7df3139cf42
fathy <fathy@FATHYALI>
2025-07-26 18:54:00 +0300

    add layout for admin and make files js,css

---
+++ ShopEgypt.Web/wwwroot/Admin/scripts.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    let toggleBtn = document.querySelector('.toggle-btn');
+
+    console.log(`btn was pressed`);
+    toggleBtn.addEventListener('click', function () {
+        const sidebar = document.getElementById('sidebar');
+        const content = document.getElementById('main-content');
+        sidebar.classList.toggle('collapsed');
+        content.classList.toggle('expanded');
+    });
+});
+
+
