94958291428f1e6456260e35f9491e4b7c1413b2
fathy <fathy@FATHYALI>
2025-07-29 02:48:31 +0300

    make update at nullable at category

--- Web.Entites/Models/Category.cs
+++ Web.Entites/Models/Category.cs
@@ -5,7 +5,7 @@
 		public int Id { get; set; }
 		public string Name { get; set; } = string.Empty;
 		public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
-		public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+		public DateTime ?UpdatedAt { get; set; } 
         public string ImageName { get; set; } = string.Empty;
 		public ICollection<Product> Products { get; set; } = [];
     }
