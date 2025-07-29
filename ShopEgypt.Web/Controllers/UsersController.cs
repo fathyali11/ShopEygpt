ff8a35d532e1300cc312e73e0f83ebca82ba1610
fathy <fathy@FATHYALI>
2025-07-29 20:33:08 +0300

    add Create user

--- ShopEgypt.Web/Controllers/UsersController.cs
+++ ShopEgypt.Web/Controllers/UsersController.cs
@@ -1,8 +1,25 @@
-﻿namespace ShopEgypt.Web.Controllers
+﻿using System.Runtime.CompilerServices;
+using Web.Entites.ViewModels.UsersVMs;
+
+namespace ShopEgypt.Web.Controllers
 {
-    public class UsersController : Controller
+    public class UsersController(IAuthRepository _authRepository) : Controller
     {
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RegisterVM request,CancellationToken cancellationToken)
+        {
+            var result=await _authRepository.RegisterAsync(request,cancellationToken);
+            if (result.IsT1)
+                return View("Register");
+
+            result.AsT0.ForEach(error =>
+            {
+                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            });
+
+            return View("Register", request);
+        }
 
     }
 }
