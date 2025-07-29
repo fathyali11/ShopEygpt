b2f6fdbc850778bb447a7f88f036abee2515b25e
fathy <fathy@FATHYALI>
2025-07-29 19:05:44 +0300

    remove old roles

--- Web.Entites/Consts/SD.cs
+++ Web.Entites/Consts/SD.cs
@@ -2,8 +2,6 @@
 {
     public static class SD
     {
-        public const string AdminRole = "Admin";
-        public const string CustomerRole = "Customer";
         public const string ImagePathProducts = "Images/Products";
         public const string ImagePathCategories= "Images/Categories";
         public const string SessionKey = "ShoppingCartNumber";
