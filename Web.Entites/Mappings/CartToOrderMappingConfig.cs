b708a6f715adf1bfddd798dbd705e967e4723084
fathy <fathy@FATHYALI>
2025-08-03 22:13:43 +0300

    add class to  map between cart and order

---
+++ Web.Entites/Mappings/CartToOrderMappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using Web.Entites.Models;
+
+namespace Web.Entites.Mappings;
+public class CartToOrderMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Cart, Order>()
+            .Map(dest => dest.TotalPrice, src => src.TotalPrice)
+            .Map(dest => dest.OrderItems, src => src.CartItems.Adapt<List<OrderItem>>());
+
+        config.NewConfig<CartItem, OrderItem>()
+            .Map(dest => dest.ProductId, src => src.ProductId)
+            .Map(dest => dest.ProductName, src => src.ProductName)
+            .Map(dest => dest.ImageName, src => src.ImageName)
+            .Map(dest => dest.UnitPrice, src => src.Price)
+            .Map(dest => dest.Quantity, src => src.Count);
+    }
+}
