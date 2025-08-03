978cb9cd670992970f419d57848fdee47e1488c6
fathy <fathy@FATHYALI>
2025-08-03 22:15:08 +0300

    update order status and payment status

--- Web.Entites/Consts/OrderStatus.cs
+++ Web.Entites/Consts/OrderStatus.cs
@@ -2,19 +2,12 @@
 
 public static class OrderStatus
 {
-    public const string StatusPending = "Pending";//قيد الانتظار
-    public const string StatusApproved = "Approved";//موافقه
-    public const string StatusInProcess = "Processing";//تعالج
-    public const string StatusShipped = "Shipped";//تم شحنها
-    public const string StatusCancelled = "Cancelled";//تم الالغاء
-    public const string StatusRefunded = "Refunded";//تم ردها
+    public const string Pending = nameof(Pending);
+    public const string Approved = nameof(Approved);
+    public const string InProcess = nameof(InProcess);
+    public const string Shipped = nameof(Shipped);
+    public const string Cancelled = nameof(Cancelled);
+    public const string Refunded = nameof(Refunded);
+    public const string Paid =nameof(Paid);
 
-    
-}
-public static class PaymentStatus
-{
-    public const string PaymentStatusPaid = "Paid";
-    public const string PaymentStatusPending = "Pending";
-    public const string PaymentStatusFailed = "Failed";
-    public const string PaymentStatusRejected = "Rejected";
 }
