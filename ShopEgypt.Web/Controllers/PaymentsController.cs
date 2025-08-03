b0af0bf6f63883a20249ab6eaca208d995f5129c
fathy <fathy@FATHYALI>
2025-08-03 12:56:43 +0300

    create PaymentsController

---
+++ ShopEgypt.Web/Controllers/PaymentsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopEgypt.Web.Controllers;
+public class PaymentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
