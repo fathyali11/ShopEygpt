c1aa141bab314840ba7f92eb373c8770cc045939
fathy <fathy@FATHYALI>
2025-08-03 14:29:50 +0300

    make relation cascade

--- Web.DataAccess/Data/EntitiesConfigurations/CartItemConfiguration.cs
+++ Web.DataAccess/Data/EntitiesConfigurations/CartItemConfiguration.cs
@@ -6,13 +6,24 @@ internal class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
 {
     public void Configure(EntityTypeBuilder<CartItem> builder)
     {
-        builder.HasKey(c => c.Id);
+        builder.HasKey(ci => ci.Id);
 
-        builder.Property(c => c.ProductId)
+        builder.Property(ci => ci.ProductName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(ci => ci.ImageName)
+            .HasMaxLength(100);
+
+        builder.Property(ci => ci.Count)
             .IsRequired();
 
-        builder.Property(c => c.CartId)
-           .IsRequired();
+        builder.Property(ci => ci.Price)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Ignore(ci => ci.TotalPrice); 
+
 
     }
 }
