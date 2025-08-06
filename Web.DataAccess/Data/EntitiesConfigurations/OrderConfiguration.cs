80eac002c2e720e0c33f994b986656fa7d7a4319
fathy <fathy@FATHYALI>
2025-08-07 02:12:29 +0300

    change places of cartconfigs

---
+++ Web.DataAccess/Data/EntitiesConfigurations/OrderConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Web.DataAccess.Data.EntitiesConfigurations;
+
+internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.UserId)
+            .IsRequired();
+
+        builder.Property(c => c.StripeSessionId)
+            .IsRequired();
+
+        builder.Property(c => c.PaymentIntentId)
+            .IsRequired();
+
+        builder.Property(c => c.Status)
+            .IsRequired();
+
+        builder.Property(c => c.TotalPrice)
+            .HasColumnType("decimal(18,2)")
+            .HasDefaultValue(0.0m);
+
+        builder.HasMany(c => c.OrderItems)
+            .WithOne(c => c.Order)
+            .HasForeignKey(p => p.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
