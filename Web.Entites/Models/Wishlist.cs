61d6caff0ea356b9ec24014f2e55dfada5ab685f
fathy <fathy@FATHYALI>
2025-08-07 12:28:49 +0300

    add wishlist and wishlistItems

---
+++ Web.Entites/Models/Wishlist.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Entites.Models;
+
+public class Wishlist
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    public List<WishlistItem> WishlistItems { get; set; } = [];
+}
+
---
+++ Web.Entites/Models/WishlistItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Entites.Models;
+
+public class WishlistItem
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string ImageName { get; set; } = string.Empty;
+    public int WishlistId { get; set; }
+    public decimal Price { get; set; } = 0.0m;
+
+    public Wishlist Wishlist { get; set; } = default!;
+}
