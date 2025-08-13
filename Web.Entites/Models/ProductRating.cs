8773f1245731bfc05961b8849c0acde75bb955cd
fathy <fathy@FATHYALI>
2025-08-14 02:36:50 +0300

    add updatedAt

--- Web.Entites/Models/ProductRating.cs
+++ Web.Entites/Models/ProductRating.cs
@@ -4,5 +4,6 @@ public class ProductRating
     public string UserId { get; set; }=string.Empty;
     public int ProductId { get; set; }
     public int Rating { get; set; }
-    public DateTime CreateAt { get; set; }=DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
 }
