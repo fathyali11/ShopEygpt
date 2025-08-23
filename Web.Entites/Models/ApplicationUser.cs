465e9a25af317daac240e88e8453d305feb6a4ce
fathy <fathy@FATHYALI>
2025-08-23 22:02:19 +0300

    add isActive for user

--- Web.Entites/Models/ApplicationUser.cs
+++ Web.Entites/Models/ApplicationUser.cs
@@ -3,6 +3,7 @@ public class ApplicationUser:IdentityUser
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName {  get; set; }= string.Empty;
+    public bool IsActive { get; set; } = true;
     public ICollection<ProductRating> ProductRatings { get; set; } = [];
     public ICollection<UserRecommendation> UserRecommendations { get; set; } = [];
     public ICollection<Order> Orders { get; set; } = [];
