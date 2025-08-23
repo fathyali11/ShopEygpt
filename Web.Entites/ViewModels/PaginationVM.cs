4c66138ad1efa052e810b9a0397ff8bcce127385
fathy <fathy@FATHYALI>
2025-08-23 15:32:47 +0300

    use partial view for pagination

---
+++ Web.Entites/ViewModels/PaginationVM.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Entites.ViewModels;
+public class PaginationVM
+{
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public bool HasNextPage { get; set; }
+    public string? SearchTerm { get; set; }
+    public string? SortField { get; set; }
+    public string? SortOrder { get; set; }
+    public string Action { get; set; } = "Index";
+    public string Controller { get; set; } = "";
+}
