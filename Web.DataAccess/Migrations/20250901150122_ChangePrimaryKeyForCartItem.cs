716427049a2b4328e1a5ef2af80b24b11d489e1f
fathy <fathy@FATHYALI>
2025-09-01 23:04:04 +0300

    change primay key of cartitem

---
+++ Web.DataAccess/Migrations/20250901150122_ChangePrimaryKeyForCartItem.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePrimaryKeyForCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CartItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                columns: new[] { "CartId", "ProductId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems",
+                column: "CartId");
+        }
+    }
+}
